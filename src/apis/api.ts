d535a9805bdb1b891d7af69727cbf3bb0965099f
Nomads <thaiantran065@gmail.com>
2025-04-29 20:21:32 +0700

    update

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -5,7 +5,11 @@ const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    Accept: 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   },
+  withCredentials: true,
 });
 
 // Add a request interceptor
@@ -24,23 +28,30 @@ export const authApi = {
   login: (username: string, password: string) => {
     return api.post('/sign-in', { username, password });
   },
-  
-  register: (userData: { username: string, email: string, password: string, name: string }) => {
+
+  register: (userData: {
+    username: string;
+    email: string;
+    password: string;
+    name: string;
+  }) => {
     return api.post('/sign-up', userData);
   },
-  
+
   googleLogin: (accessToken: string) => {
-    return api.post('/auth/candidate/google/login', { credential: accessToken });
+    return api.post('/auth/candidate/google/login', {
+      credential: accessToken,
+    });
   },
-  
+
   logout: () => {
     return api.post('/auth/logout');
   },
-  
+
   getProfile: () => {
     return api.get('/user/profile');
   },
-  
+
   updateProfile: (userData: any) => {
     return api.put('/user/profile', userData);
   },
@@ -50,30 +61,30 @@ export const questionApi = {
   getQuestions: (params: any) => {
     return api.get('/questions', { params });
   },
-  
+
   getQuestionById: (id: string | number) => {
     return api.get(`/questions/${id}`);
   },
-  
+
   getRandomQuestions: () => {
     return api.get('/home/rand-question');
   },
-  
+
   getCategories: () => {
     return api.get('/questions/categories');
   },
-  
+
   getTopPlayers: () => {
     return api.get(`/user/top-10-scores`);
   },
-  
+
   getTopScores: () => {
     return api.get('/user/top-scores');
   },
-  
+
   searchQuestions: (params: any) => {
     return api.get('/questions/search', { params });
-  }
+  },
 };
 
-export default api; 
+export default api;
